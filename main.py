90ea9b6184d0f29349b9496a528a118a7be84073
AnamariaCiocan <91565562+AnamariaCiocan@users.noreply.github.com>
2021-10-12 12:14:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ def is_palindrome(n):
 
     '''
     x=0
+    k=0
     cn=n
-    while n!=0:
-        x*=10+n%10
+    while n>0:
+        k=n%10
+        x=x*10+k
         n=n//10
     if cn==x:
         return True
-    elif: 
+    else:
         return False
 
 
@@ -38,22 +40,46 @@ def get_n_choose_k(n, k):
     for i in range(2,n-k+1):
         z*=i
     v=x//y
-    return v/z
+    return v//z
 
- def test_get_n_choose_k():
-     assert get_n_choose_k(3,4) == 4
-     assert get_n_choose_k(2,5) == 10
-     assert get_n_choose_k(2,6) == 15
+def test_get_n_choose_k():
+     assert get_n_choose_k(4,3) == 4
+     assert get_n_choose_k(5,2) == 10
+     assert get_n_choose_k(6,2) == 15
      assert get_n_choose_k(4,4) == 1
      
 
-   
+def is_superprime(n):
+    '''
+    Verifica daca un numar si toate prefixele sale sunt numere prime si returneaza True/False dupa caz.
+    n-numar intreg
+    '''
+    if n<2:
+        return False
+    else:
+     while n>1:
+        if n<2:
+            return False
+        for i in range(2,n):
+            if n%i == 0:
+                return False
+        n=n//10
+    return True
+
+def test_is_superprime():
+    assert is_superprime(233) == True
+    assert is_superprime(237) == False
+    assert is_superprime(24) == False
+    assert is_superprime(17) == True
+
+
 
 def main():
     while True:
         print("1.Verificati daca un numar este palindrom")
         print("2.Determinati combinari de n luate cate k")
-        print("3.Iesire")
+        print("3.Verificati daca un numar este superprim")
+        print("4.Iesire")
         optiune = input("Alegeti optiune: ")
         if optiune == '1':
             n=int(input("Alegeti un numar pentru a verifica daca e palindrom: "))
@@ -67,8 +93,14 @@ def main():
             print("Rezultatul este: ")
             print(get_n_choose_k(n,k))
         elif optiune == '3':
+            n=int(input("Alegeti un numar pentru a verifica daca e superprim: "))
+            if is_superprime(n):
+                print("Este superprim")
+            else:
+                print("Nu este superprim")
+        elif optiune == '4':
              break
 
 
-
-main()
+if __name__ == '__main__':
+    main()
